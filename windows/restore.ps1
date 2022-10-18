87b1b7c22d33b542a4d9876470a0497bc8461aa9
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-10-19 01:13:57 +0200

    fix(windows): fix restore script for vscode extensions

--- windows/restore.ps1
+++ windows/restore.ps1
@@ -2,6 +2,6 @@
 Copy-Item .\home\Code\User\settings.json $env:APPDATA\Code\User\settings.json
 
 # vscode > extensions
-foreach($extension in Get-Content .\home\Code\extensions-list) {
+foreach($extension in Get-Content .\home\Code\extensions.txt) {
     code --install-extension $extension
 }
